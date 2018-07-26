e45df5997f92978d3363649aee61d1a7f2e587b5
Karissa Bui <karissab002@gmail.com>
2018-07-26 16:59:53 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -9,17 +9,21 @@ def least_coins(cents)
     :pennies = 0
   }
   #quarters
-  while cents >= 25
+while cents >= 25
     coins[:quarters] += 1 
     cents = cents - 25
   end
  #dimes
- while cents >= 10 
+while cents >= 10 
   coins[:dimes] += 1 
   cents = cents - 10
 end
-  
   #nickles
+while cents >= 5 
+  coins[:nickles] += 1 
+  cents = cents - 5 
+end 
+
   
   #pennies
   
