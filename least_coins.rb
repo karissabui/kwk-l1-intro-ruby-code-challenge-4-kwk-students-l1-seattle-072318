6ef46ef597bfc5cb56bbb8a50433d5edf4c7846f
Karissa Bui <karissab002@gmail.com>
2018-07-26 16:49:48 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,28 @@
-#write out your code here
 
 def least_coins(cents)
 
-#Code your answer here!
+least_coins = {
+  :quarters => 0,
+  :dimes => 0,
+  :nickles => 0,
+  :pennies => 0,
+}
 
+
+    if least_coins >= 25
+     least_coins % 25
 end
+
+  if least_coins >= 10 
+   least_coins % 10
+end
+
+  if least_coins >= 5 
+    least_coins % 5 
+end
+
+  if least_coins >= 1 
+    least_coins % 1
+end 
+
+  
